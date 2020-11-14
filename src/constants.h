bb84b130c84242ba13082880e739b93d1fa4a543
Gonzalez Laura Sofia <lsgonzal@INTRANET.EPFL.CH@SVUbuUniv15.intranet.epfl.ch>
2020-11-14 19:32:53 +0100

    constantes pour la gestion d erreurs et TCLAP

---
+++ src/constants.h
@@ -0,0 +1,32 @@
+
+/*!
+  Une classe pour lancer les erreurs dans le programme.
+  Elle hérite d'une sous-classe de la classe std::exception.
+  Chaque classe d'erreurs a un code de sortie spécifique.
+  Les messages d'erreurs sont affichés par l'appeleur d'exceptions. 
+*/
+
+class Erreur : public std::runtime_error {	
+public:
+    Erreur (const char *c, int v=0) : std::runtime_error(c), code(v) {}
+    Erreur (const std::string &s, int v=0) : std::runtime_error(s), code(v) {}  
+    int value() const {return code;}
+    
+protected:
+    const int code
+};
+
+
+/// * valeurs pas défaut des paramètres *
+#define _TEMPS_DEFAUT_ 10
+#define _N_DEFAUT_ 100
+#define _PROPORTIONS_DEFAUT_"RS:0,8, FS:0,2"
+
+/// * messages *
+#define _PRGRM_TEXTE_ "Simulation du réseau de neurones"
+#define _TEMPS_TEXTE_ "Durée en ms de la simulation"
+#define _N_NEUR_TEXTE_ "Nombre de neurones dans la simulation"
+#define _PROPORTIONS_TEXTE_ "Proportions des différents neurones (écrire RS: x , FS: y avec x et y les proportions respectives)" //spécifier ensuite RS, IB, CH, FS et LTS
+
+
+
