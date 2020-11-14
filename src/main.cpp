c29092edbf9672dc7793d3a462049f887152bbf0
Gonzalez Laura Sofia <lsgonzal@INTRANET.EPFL.CH@SVUbuUniv15.intranet.epfl.ch>
2020-11-14 19:34:35 +0100

    fichiers vides

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "Simulation.cpp"
+
+int main(){
+
+	//Juste pour pour tester le TCLAP
+    std::cout<<" Le temps choisi est "<<temps.getValue()<<" , le nombre de neurones est "<<nb_neur.getValue()<<" et les proportions sont "<<proportions.getValue()<<std::endl;
+
+return 0;
+};
