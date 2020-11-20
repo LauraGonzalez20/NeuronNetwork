8ac2edd905d9998415e2bb8b0e0482274bbba07b
Laura Gonzalez <laura.gonzalez@epfl.ch>
2020-11-20 21:05:06 +0100

    Classe Random completee

--- src/Random.cpp
+++ src/Random.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include "Random.h"
 
+Random::Random() 
+{
+   std::random_device rd;
+   seed = rd();
+        
+   rng.seed(seed); //?
+}
+
+int Random::uniform(int lower,int upper) {
+    std::uniform_int_distribution<> unif(lower, upper);
+    return unif(rng); 
+}
+
+double Random::normal(double moy, double sd) {
+    std::normal_distribution<> norm(moy, sd);
+    return norm(rng);
+}
+
+int Random::poisson(int lambda)
+{
+	std::poisson_distribution<> pd(lambda);
+	return pd(rng);
+}
+    
