73b24ee2fd9986a8d9de69485baf148aeda2395f
Alois <alois.thomas@epfl.ch>
2020-11-20 21:58:07 +0100

    complétion main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include "Simulation.cpp"
 
-int main(){
-
+int main(int argc, char **argv){
+    try{
+        Simulation s(argc, argv);
+        s.run();
+    }catch(Erreur& e){
+       std::cerr<<"Error: " + e.error() + " " + e.argId();
+    }
     
 return 0;
-};
+}
